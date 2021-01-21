9d33084fc5ef95df7c8726453940c4128acda9ee
MamadovZaki <mamadovzicx@gmail.com>
2021-01-21 10:33:33 +0200

    moved <Router></Router> to App.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-   <Router>
-      <App />
-   </Router>,
-   document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
