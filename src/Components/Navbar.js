1cbbe41d8e4a9f94ba6cf4a82e6a972e81f213ff
MamadovZaki <mamadovzicx@gmail.com>
2021-01-21 12:20:34 +0200

    added links to the navbar and un-commented the 2 css lines -- dropdown menu works and toggle button is working

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "../images/logo.svg";
-import { FaAllignRight } from "react-icons/fa";
+import { FaAlignRight } from "react-icons/fa";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default class Navbar extends Component {
@@ -15,10 +15,34 @@ export default class Navbar extends Component {
             <div className="nav-center">
                <div className="nav-header">
                   <Router>
-                     <Link to="/" />
+                     <Link to="/">
+                        <img src={logo} alt="beach resort" />
+                     </Link>
                   </Router>
-                  <img src={logo} alt="beach resort" />
+                  <button
+                     type="button"
+                     className="nav-btn"
+                     onClick={this.handleToggle}
+                  >
+                     <FaAlignRight className="nav-icon" />
+                  </button>
                </div>
+               <ul
+                  className={
+                     this.state.isOpen ? "nav-links show-nav" : "nav-links"
+                  }
+               >
+                  <li>
+                     <Router>
+                        <Link to="/">Home</Link>
+                     </Router>
+                  </li>
+                  <li>
+                     <Router>
+                        <Link to="/rooms">Rooms</Link>
+                     </Router>
+                  </li>
+               </ul>
             </div>
          </nav>
       );
