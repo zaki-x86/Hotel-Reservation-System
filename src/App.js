4fcb0bdaac44910c1eac4ed04b594c94035f02ab
MamadovZaki <mamadovzicx@gmail.com>
2021-01-21 06:59:58 +0200

    added react-router-dom to App.js, see docs at: https://reactrouter.com/web/guides/quick-start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
-import Welcome from "./Components/welcome";
 import "./App.css";
 import Home from "./Pages/Home";
 import Rooms from "./Pages/Rooms";
 import SingleRoom from "./Pages/SingleRoom";
 import Error from "./Pages/Error";
+import { Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
    return (
       <>
-         <Welcome />
-         <Home />
-         <Rooms />
-         <SingleRoom />
-         <Error />
+         <Switch>
+            <Route exact path="/rooms">
+               <Rooms />
+            </Route>
+            <Route exact path="/single-room">
+               <SingleRoom />
+            </Route>
+            <Route exact path="/">
+               <Home />
+            </Route>
+         </Switch>
       </>
    );
 };
