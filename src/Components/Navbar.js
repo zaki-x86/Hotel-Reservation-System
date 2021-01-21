0bf00063ca8c5172940d6ffaed65db187043d3e5
MamadovZaki <mamadovzicx@gmail.com>
2021-01-21 16:26:54 +0200

    changed the structure of <Router> </Router/>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,40 +11,36 @@ export default class Navbar extends Component {
    handleToggle = () => this.setState({ isOpen: !this.state.isOpen });
    render() {
       return (
-         <nav className="navbar">
-            <div className="nav-center">
-               <div className="nav-header">
-                  <Router>
+         <Router>
+            <nav className="navbar">
+               <div className="nav-center">
+                  <div className="nav-header">
                      <Link to="/">
                         <img src={logo} alt="beach resort" />
                      </Link>
-                  </Router>
-                  <button
-                     type="button"
-                     className="nav-btn"
-                     onClick={this.handleToggle}
+                     <button
+                        type="button"
+                        className="nav-btn"
+                        onClick={this.handleToggle}
+                     >
+                        <FaAlignRight className="nav-icon" />
+                     </button>
+                  </div>
+                  <ul
+                     className={
+                        this.state.isOpen ? "nav-links show-nav" : "nav-links"
+                     }
                   >
-                     <FaAlignRight className="nav-icon" />
-                  </button>
-               </div>
-               <ul
-                  className={
-                     this.state.isOpen ? "nav-links show-nav" : "nav-links"
-                  }
-               >
-                  <li>
-                     <Router>
+                     <li>
                         <Link to="/">Home</Link>
-                     </Router>
-                  </li>
-                  <li>
-                     <Router>
+                     </li>
+                     <li>
                         <Link to="/rooms">Rooms</Link>
-                     </Router>
-                  </li>
-               </ul>
-            </div>
-         </nav>
+                     </li>
+                  </ul>
+               </div>
+            </nav>
+         </Router>
       );
    }
 }
