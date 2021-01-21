4429171014710ba1e7c81e93963ce51660caef55
MamadovZaki <mamadovzicx@gmail.com>
2021-01-21 05:44:45 +0200

    imported Pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Welcome from "./Components/welcome";
 import "./App.css";
+import Home from "./Pages/Home";
+import Rooms from "./Pages/Rooms";
+import SingleRoom from "./Pages/SingleRoom";
+import Error from "./Pages/Error";
 
 const App = () => {
-   return <Welcome />;
+   return (
+      <>
+         <Welcome />
+         <Home />
+         <Rooms />
+         <SingleRoom />
+         <Error />
+      </>
+   );
 };
 
 export default App;
