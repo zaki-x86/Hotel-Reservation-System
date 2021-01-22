bb7ddaeb9d8892384f31b551dc5ab7d55203aebf
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 16:27:11 +0200

    Prepared data and logic in context

--- src/context.js
+++ src/context.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-
+import items from "./data";
 const RoomContext = React.createContext(/*initial value here */);
 
 class RoomProvider extends Component {
    state = {
-      greeting: "hello",
-      name: "zaki",
+      rooms: [],
+      sortedRooms: [],
+      featuredRooms: [],
+      loading: true,
    };
+
+   componentDidMount() {
+      let rooms = this.formatData(items);
+      let featuredRooms = rooms.filter((room) => room.featured === true);
+      this.setState({
+         rooms,
+         featuredRooms,
+         sortedrRooms: rooms,
+         loading: false,
+      });
+   }
+
+   formatData = (items) => {
+      let tempItems = items.map((item) => {
+         let id = item.sys.id;
+         let images = item.fields.images.map((image) => image.fields.file.url);
+         let room = { ...item.fields, images, id };
+         return room;
+      });
+      return tempItems;
+   };
+
    render() {
       return (
          <RoomContext.Provider value={{ ...this.state }}>
