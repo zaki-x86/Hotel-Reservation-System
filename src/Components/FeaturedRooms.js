110157046ecf4f0c95ad65649fca9ddef399285f
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 16:35:37 +0200

    [Loading]Testing new component

--- src/Components/FeaturedRooms.js
+++ src/Components/FeaturedRooms.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
-
+import Loading from "../Components/Loading";
 export default class FeaturedRooms extends Component {
    static contextType = RoomContext;
    render() {
-      const {} = this.context;
-      console.log();
+      const { featuredRooms } = this.context;
+      console.log(featuredRooms);
       return (
          <div>
             <h1>Featured Rooms here: </h1>
+            <Loading />
          </div>
       );
    }
