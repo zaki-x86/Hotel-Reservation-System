0fec4a6e746a72a8571719afa07781bb90c186fd
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 23:51:42 +0200

    [StyledHero]Added new Component

---
+++ src/Components/StyledHero.js
@@ -0,0 +1,11 @@
+import Styled from "styled-components";
+
+/* 
+    This is for testing the components -- will be removed later
+*/
+const SimpleButton = styled.button`
+   color: red;
+   background: green;
+`;
+
+export default SimpleButton;
