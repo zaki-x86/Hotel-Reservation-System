b298da2d81d8639d3d1b73f1f2ef851d22cfaf10
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 15:50:27 +0200

    Testing context API with objects

--- src/Components/FeaturedRooms.js
+++ src/Components/FeaturedRooms.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { RoomContext } from "../context";
 
 export default class FeaturedRooms extends Component {
+   static contextType = RoomContext;
    render() {
+      const { greeting, name } = this.context;
+      console.log(value);
       return (
          <div>
-            <h1>Featured Rooms here</h1>
+            <h1>Featured Rooms here: {greeting}</h1>
          </div>
       );
    }
--- src/context.js
+++ src/context.js
@@ -3,10 +3,13 @@ import React, { Component } from "react";
 const RoomContext = React.createContext(/*initial value here */);
 
 class RoomProvider extends Component {
-   state = {};
+   state = {
+      greeting: "hello",
+      name: "zaki",
+   };
    render() {
       return (
-         <RoomContext.Provider value={"hello"}>
+         <RoomContext.Provider value={{ ...this.state }}>
             {this.props.children}
          </RoomContext.Provider>
       );
