e031a3b3867ecbc589844c5537ae842c52e771f8
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 15:29:15 +0200

    Setup boilerplate for new component

---
+++ src/Components/FeaturedRooms.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class FeaturedRooms extends Component {
+   render() {
+      return (
+         <div>
+            <h1>Featured Rooms here</h1>
+         </div>
+      );
+   }
+}
