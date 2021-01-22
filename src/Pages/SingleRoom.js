40e97d4628b2680a7e3ecacc2d8647840d474ff0
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 10:55:29 +0200

    Attempt to fix #6

--- src/Pages/SingleRoom.js
+++ src/Pages/SingleRoom.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class SingleRoom extends Component {
-   render() {
-      return (
-         <div>
-            <h1>Single Room Page</h1>
-         </div>
-      );
-   }
+function SingleRoom() {
+   return (
+      <div>
+         <h1>single</h1>
+      </div>
+   );
 }
+
+export default SingleRoom;
