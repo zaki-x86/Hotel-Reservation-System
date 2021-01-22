d60b48f403147365247b639e098c712e2ae13068
MamadovZaki <mamadovzicx@gmail.com>
2021-01-22 16:48:03 +0200

    [FeaturedRooms]Setup  Rooms and Loading inside

--- src/Components/FeaturedRooms.js
+++ src/Components/FeaturedRooms.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
 import Loading from "../Components/Loading";
+import Room from "../Components/Room";
+import Title from "./Title";
+
 export default class FeaturedRooms extends Component {
    static contextType = RoomContext;
    render() {
-      const { featuredRooms } = this.context;
-      console.log(featuredRooms);
+      let { featuredRooms, loading } = this.context;
+      //console.log(featuredRooms);
+      let rooms = featuredRooms.map((room) => {
+         return <Room key={room.id} room={room} />;
+      });
+
       return (
-         <div>
-            <h1>Featured Rooms here: </h1>
-            <Loading />
-         </div>
+         <section className="featured-rooms">
+            <Title title="feature rooms" />
+            <div className="featured-rooms-center">
+               {loading ? <Loading /> : rooms}
+            </div>
+         </section>
       );
    }
 }
