6e914b0afdf86761315a4485ffe76a4dcee0141c
MamadovZaki <mamadovzicx@gmail.com>
2021-01-23 08:54:47 +0200

    [RoomsContainer.js]Finalized component view
    Extracted data from context.js using react hook and passed it to RoomsFilter
    and RoomsList.

--- src/Components/RoomsContainer.js
+++ src/Components/RoomsContainer.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
+import { RoomContext } from "../context";
+import Loading from "./Loading";
 
 const RoomsContainer = () => {
+   let context = useContext(RoomContext);
+   /* When you console log context, you will receive 2 log. First, is a value object with all state variables grabbed from context.js, but they are empty arrays. Second, is a again a value object with all state variables grabbed from context.js but the arrays will not be empty */
+   //console.log(context);
+   const { loading, sortedRooms, rooms } = context;
+   if (loading) {
+      return <Loading />;
+   }
    return (
-      <div>
+      <>
          <h1>Rooms Container</h1>
-         <RoomsFilter />
-         <RoomsList />
-      </div>
+         <RoomsFilter rooms={rooms} />
+         <RoomsList rooms={sortedRooms} />
+      </>
    );
 };
 
