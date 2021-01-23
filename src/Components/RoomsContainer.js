4383c67f2164c17a8b1254e4600ca55b0e1857c1
MamadovZaki <mamadovzicx@gmail.com>
2021-01-23 10:45:23 +0200

    [RoomsContainer.js]removed testing code

--- src/Components/RoomsContainer.js
+++ src/Components/RoomsContainer.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
-import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
+import RoomsFilter from "./RoomsFilter";
 import { RoomContext } from "../context";
 import Loading from "./Loading";
 
 const RoomsContainer = () => {
    let context = useContext(RoomContext);
-   /* When you console log context, you will receive 2 log. First, is a value object with all state variables grabbed from context.js, but they are empty arrays. Second, is a again a value object with all state variables grabbed from context.js but the arrays will not be empty */
    //console.log(context);
    const { loading, sortedRooms, rooms } = context;
    if (loading) {
